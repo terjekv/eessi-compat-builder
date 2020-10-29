808aa5aa4fb2a37f2ba84309dd66445708747798
Terje Kvernes <terje@kvernes.no>
2020-10-29 19:16:00 +0100

    Fix logging properly.

--- terraform/remote.sh
+++ terraform/remote.sh
@@ -5,12 +5,16 @@ sudo yum -y update
 sudo yum -y upgrade
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo yum -y install singularity
-sudo yum -y install tee
+
+mkdir ~/compat/
+LOGFILE=~/compat/$(uname -m).build.log
+
+touch $LOGFILE
 
 (
-    echo "Initalizing compat layer build for $( uname -a ) on a $( curl http://169.254.169.254/latest/meta-data/instance-type )"
+    echo "Initalizing compat layer build for $( uname -m ) on a $( curl http://169.254.169.254/latest/meta-data/instance-type )"
     echo "Starting build: $(date)"
     time singularity run docker://eessi/bootstrap-prefix:centos8-$(uname -m) ~/compat/$(uname -m) noninteractive 
     echo "Finished build: $( date )"
-) | tee ~/compat/$(uname -m).build.log
+) |tee -a $LOGFILE
 
